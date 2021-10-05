11391180b22e61ca3b87c1868334571e08d7eaaa
Darlan Thiago <darlanthiago53@gmail.com>
2021-10-05 17:51:29 -0300

    Add var context

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,12 @@
 import { AppLayout } from "../components/Layout/app";
+import { useReactAuth } from "../hooks/AuthContext";
 import { AppRoutes } from "./app.routes";
 import { PublicRoutes } from "./public.routes";
 
 export const Routes = () => {
-  const isSigned = true;
+  const { isSigned } = useReactAuth();
+
+  console.log(isSigned);
 
   return isSigned ? (
     <>
