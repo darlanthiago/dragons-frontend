f4d8a5d75d90088775ab522b35464ad9f6f76474
Darlan Thiago <darlanthiago53@gmail.com>
2021-10-05 16:04:16 -0300

    Remove Flex

--- src/components/Layout/app.tsx
+++ src/components/Layout/app.tsx
@@ -1,20 +1,11 @@
-import { Flex } from "@chakra-ui/react";
 import { Header } from "../Header";
 
 export const AppLayout: React.FC = ({ children }) => {
   return (
     <>
       <Header />
-      <Flex
-        w="100%"
-        paddingY="10"
-        marginX="auto"
-        px="6"
-        bg="gray.100"
-        height="100%"
-      >
-        {children}
-      </Flex>
+
+      {children}
     </>
   );
 };
